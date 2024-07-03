3eb07fd7294d37a3a728c0ac57251cb196b83546
Wojtsik <72934201+Wojtsik@users.noreply.github.com>
2024-07-03 20:35:05 +0200

    Usunięto reload bo i tak nie działał

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import discord
 import os
-TOKEN = 'tuidzietoken'
+TOKEN = 'TOKEN'
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = discord.Bot(intents=intents, activity = discord.Game(name="Test1234"))
+bot = discord.Bot(intents=intents, activity = discord.Game(name="Sasaki Alpha"))
 
 
 @bot.event #ładowanie komend w folderze /cogs
@@ -17,15 +17,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'): 
         bot.load_extension(f'cogs.{filename[:-3]}')
         print(f"Załadowano: {filename[:-3]}")
-    else:
-        print("Nie znaleziono więcej plików .py")
 
-@bot.slash_command() #teoretcznie powinno przeładować cogi bez wyłączania bota
-async def reload(ctx):
-    if filename.endswith('.py'): 
-        bot.unload_extension(f'cogs.{filename[:-3]}')
-    if filename.endswith('.py'): 
-        bot.load_extension(f'cogs.{filename[:-3]}')
-    await ctx.respond('przeładowano')
 
 bot.run(TOKEN)
