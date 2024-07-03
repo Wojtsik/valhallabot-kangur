0fc5160131069a922b93cd2b78d43ce148a46a6e
Wojtsik <72934201+Wojtsik@users.noreply.github.com>
2024-07-03 20:35:57 +0200

    Dodano wybór serwera (cn lub pl)

--- cogs/lootcog.py
+++ cogs/lootcog.py
@@ -10,12 +10,15 @@ class lupy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="lupy", description="Wysyła łupy danego gracza", guild_ids=[1257688368767176785])
+    @commands.slash_command(name="lupy", description="Wysyła łupy danego gracza")
 
-    async def lupy(self, ctx: discord.ApplicationContext, gracz: Option(str, "Wpisz nazwę gracza")): # type: ignore
-
-        link = (f'https://empire-api.fly.dev/EmpireEx_5/hgh/"LT":2,"SV":"{gracz}"')
-        print(link)
+    async def lupy(self, ctx: discord.ApplicationContext, gracz: Option(str, "Wpisz nazwę gracza"), serwer: Option(str, "pl, cn", choices=["cn", "pl"])): # type: ignore
+        if serwer.lower() == "cn":
+            link = (f'https://empire-api.fly.dev/EmpireEx_27/hgh/"LT":2,"SV":"{gracz}"')
+        elif serwer.lower() == "pl":
+            link = (f'https://empire-api.fly.dev/EmpireEx_5/hgh/"LT":2,"SV":"{gracz}"')
+        else:
+            await ctx.respond("Dostępne opcje to pl (Polska 1) lub cn (Chiński)")
         json_download = requests.get(link)
         todos = json.loads(json_download.text)
         rank = todos['content']['FR']   
@@ -24,6 +27,6 @@ class lupy(commands.Cog):
             if rank==todos['content']['L'][i][0]:
                 lupy = todos['content']['L'][i][1]
                 formattedlupy = f"{lupy:,d}".replace(',', ' ')
-        await ctx.respond(f'Gracz {gracz1} w tym tygodniu pozyskał {formattedlupy} punktów rabunku')
+        await ctx.respond(f'Gracz **{gracz1}** w tym tygodniu pozyskał **{formattedlupy}** punktów rabunku', ephemeral=False)
 def setup(bot):
     bot.add_cog(lupy(bot))
